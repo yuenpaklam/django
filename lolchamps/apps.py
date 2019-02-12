24f6a04de42ffd9dee47d0272b93805199e75f21
paklam <paklamyuen@gmail.com>
2019-02-12 22:34:40 +0800

    '123'

--- lolchamps/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LolchampsConfig(AppConfig):
-    name = 'lolchamps'
--- lolchamps/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-def content_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
-class Champs(models.Model):
-    champ_name = models.CharField(max_length=100)
-    champ_image = models.ImageField(upload_to=content_path)
-
-
